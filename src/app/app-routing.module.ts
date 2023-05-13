343b406684edc3dc1a7bcbfdd5459b8350448f1e
Eric Mayer <eric.araujo_mayer@hotmail.com>
2023-05-13 10:45:02 -0300

    feat: criado estrutura inicial do projeto
    Criado estrutura inicial do projeto com configuração eslint, prettier, docker, commitlint, husky,
    standard-version, arquivos scss, etc.
    
    BREAKING CHANGE: Criado estrutura inicial do projeto com configuração eslint, prettier, docker,
    commitlint, husky, standard-version, arquivos scss, etc.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes, withComponentInputBinding())
+  ]
 })
 export class AppRoutingModule { }
